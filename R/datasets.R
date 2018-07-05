d40be39190a66d89bcbcc863fbcc28080e781539
Alfredo Hernández <aldomann.designs@gmail.com>
2018-07-05 11:04:13 +0200

    Finished documentation.

--- R/datasets.R
+++ R/datasets.R
@@ -1,6 +1,7 @@
 #' Hurricane Observations from the NATL and EPAC basins
 #'
-#' A dataset containing summarised hurricane/tropical-cyclone observations from the North Atlantic (1966-2016) and Northeast Pacific (1986-2016) basins.
+#' A dataset containing summarised hurricane/tropical-cyclone observations from the
+#' North Atlantic (1966-2016) and Northeast Pacific (1986-2016) basins.
 #'
 #' The variables are as follows:
 #'
@@ -20,7 +21,8 @@
 #'   \item sst.class: low for sst.norm < 1, high for sst.norm >= 1.
 #' }
 #'
-#' The hurricane data (variables 1-9) comes from the HURDAT2, while the SST is calculated from the HadISST1 (variables 11-13).
+#' The hurricane data (variables 1-9) comes from the HURDAT2, while the SST is calculated
+#' from the HadISST1 (variables 11-13).
 #'
 #' @docType data
 #' @keywords datasets
@@ -33,7 +35,8 @@ NULL
 
 #' Hurricane Observations from the NATL basin
 #'
-#' A dataset containing summarised hurricane/tropical-cyclone observations from the North Atlantic (1966-2016) basin.
+#' A dataset containing summarised hurricane/tropical-cyclone observations from the
+#' North Atlantic (1966-2016) basin.
 #'
 #' The variables are as follows:
 #'
@@ -53,13 +56,14 @@ NULL
 #'   \item sst.class: low for sst.norm < 1, high for sst.norm >= 1.
 #' }
 #'
-#' The hurricane data (variables 1-9) comes from the HURDAT2, while the SST is calculated from the HadISST1 data (variables 11-13).
+#' The hurricane data (variables 1-9) comes from the HURDAT2, while the SST is calculated
+#' from the HadISST1 data (variables 11-13).
 #'
 #' @docType data
 #' @keywords datasets
 #' @name tc.pdi.natl
 #' @usage data(tc.pdi.natl)
-#' @format A data frame with 1365 rows and 13 variables
+#' @format A data frame with 771 rows and 13 variables
 #' @author Alfredo Hernández \email{aldomann.designs@gmail.com}
 #' @references HURDAT: \url{http://www.aoml.noaa.gov/hrd/hurdat/Data_Storm.html}, HadISST1: \url{https://www.metoffice.gov.uk/hadobs/hadisst/}
 NULL
@@ -80,21 +84,139 @@ NULL
 #'   \item mean.wind: mean of the maximum sustained surface wind speed.
 #'   \item mean.sq.wind: mean of the squared maximum sustained surface wind speed.
 #'   \item storm.year: Year of storm occurrence.
-#'   \item basin: NATL (North Atlantic), this column is kept for structure consistency.
+#'   \item basin: EPAC (Northeast Pacific), this column is kept for structure consistency.
 #'   \item sst: Annual and basin-wide averaged sea surface temperature.
 #'   \item sst.norm: Normalised SST.
 #'   \item sst.class: low for sst.norm < 1, high for sst.norm >= 1.
 #' }
 #'
-#' The hurricane data (variables 1-9) comes from the HURDAT2, while the SST is calculated from the HadISST1 data (variables 11-13).
+#' The hurricane data (variables 1-9) comes from the HURDAT2, while the SST is calculated
+#' from the HadISST1 data (variables 11-13).
 #'
 #' @docType data
 #' @keywords datasets
 #' @name tc.pdi.epac
 #' @usage data(tc.pdi.epac)
-#' @format A data frame with 1365 rows and 13 variables
+#' @format A data frame with 594 rows and 13 variables
 #' @author Alfredo Hernández \email{aldomann.designs@gmail.com}
 #' @references HURDAT: \url{http://www.aoml.noaa.gov/hrd/hurdat/Data_Storm.html}, HadISST1: \url{https://www.metoffice.gov.uk/hadobs/hadisst/}
 NULL
 
+#' Hurricane Observations from the NATL and EPAC basins with Geographical Data
+#'
+#' A dataset containing summarised hurricane/tropical-cyclone observations from the North
+#' Atlantic (1966-2016) and Northeast Pacific (1986-2016) basins with additional geographic
+#' variables.
+#'
+#' The variables are as follows:
+#'
+#' \itemize{
+#'   \item storm.id: Unique storm ID. Constructed by basin (chars. 1-2) + ATCF cyclone number for that year (chars. 3-4) + Year (chars. 5-8).
+#'   \item storm.name: Name, if available, or else "UNNAMED".
+#'   \item n.obs: Number of best track entries.
+#'   \item storm.duration: Lifetime of the storm in seconds.
+#'   \item storm.pdi: Power-dissipation index. An indicator of the released energy of a hurricane, it integrates (cubic) wind speed over tropical-cyclone lifetime.
+#'   \item max.wind: supremum of the maximum sustained surface wind speed.
+#'   \item mean.wind: mean of the maximum sustained surface wind speed.
+#'   \item mean.sq.wind: mean of the squared maximum sustained surface wind speed.
+#'   \item storm.year: Year of storm occurrence.
+#'   \item basin: NATL (North Atlantic) or EPAC (Northeast Pacific).
+#'   \item sst: Annual and basin-wide averaged sea surface temperature.
+#'   \item sst.norm: Normalised SST.
+#'   \item sst.class: low for sst.norm < 1, high for sst.norm >= 1.
+#'   \item first.lat: Genesis latitude of the storm.
+#'   \item last.lat: Death latitude of the storm.
+#'   \item first.long: Genesis longitude of the storm.
+#'   \item last.long: Death latitude of the storm.
+#'   \item distance: Path length of the storm. Calculated using the Spherical Law of Cosines.
+#' }
+#'
+#' The hurricane data (variables 1-9, 14-18) comes from the HURDAT2, while the SST is calculated from the HadISST1 (variables 11-13).
+#'
+#' @docType data
+#' @keywords datasets
+#' @name tc.pdi.geog.all
+#' @usage data(tc.pdi.geog.all)
+#' @format A data frame with 1365 rows and 18 variables
+#' @author Alfredo Hernández \email{aldomann.designs@gmail.com}
+#' @references HURDAT: \url{http://www.aoml.noaa.gov/hrd/hurdat/Data_Storm.html}, HadISST1: \url{https://www.metoffice.gov.uk/hadobs/hadisst/}
+NULL
 
+#' Hurricane Observations from the NATL basin with Geographical Data
+#'
+#' A dataset containing summarised hurricane/tropical-cyclone observations from the North
+#' Atlantic (1966-2016) basin with additional geographic
+#' variables.
+#'
+#' The variables are as follows:
+#'
+#' \itemize{
+#'   \item storm.id: Unique storm ID. Constructed by basin (chars. 1-2) + ATCF cyclone number for that year (chars. 3-4) + Year (chars. 5-8).
+#'   \item storm.name: Name, if available, or else "UNNAMED".
+#'   \item n.obs: Number of best track entries.
+#'   \item storm.duration: Lifetime of the storm in seconds.
+#'   \item storm.pdi: Power-dissipation index. An indicator of the released energy of a hurricane, it integrates (cubic) wind speed over tropical-cyclone lifetime.
+#'   \item max.wind: supremum of the maximum sustained surface wind speed.
+#'   \item mean.wind: mean of the maximum sustained surface wind speed.
+#'   \item mean.sq.wind: mean of the squared maximum sustained surface wind speed.
+#'   \item storm.year: Year of storm occurrence.
+#'   \item basin: NATL (North Atlantic), this column is kept for structure consistency.
+#'   \item sst: Annual and basin-wide averaged sea surface temperature.
+#'   \item sst.norm: Normalised SST.
+#'   \item sst.class: low for sst.norm < 1, high for sst.norm >= 1.
+#'   \item first.lat: Genesis latitude of the storm.
+#'   \item last.lat: Death latitude of the storm.
+#'   \item first.long: Genesis longitude of the storm.
+#'   \item last.long: Death latitude of the storm.
+#'   \item distance: Path length of the storm. Calculated using the Spherical Law of Cosines.
+#' }
+#'
+#' The hurricane data (variables 1-9, 14-18) comes from the HURDAT2, while the SST is calculated from the HadISST1 (variables 11-13).
+#'
+#' @docType data
+#' @keywords datasets
+#' @name tc.pdi.geog.natl
+#' @usage data(tc.pdi.geog.natl)
+#' @format A data frame with 771 rows and 18 variables
+#' @author Alfredo Hernández \email{aldomann.designs@gmail.com}
+#' @references HURDAT: \url{http://www.aoml.noaa.gov/hrd/hurdat/Data_Storm.html}, HadISST1: \url{https://www.metoffice.gov.uk/hadobs/hadisst/}
+NULL
+
+#' Hurricane Observations from the NATL and EPAC basins with Geographical Data
+#'
+#' A dataset containing summarised hurricane/tropical-cyclone observations from the Northeast
+#' Pacific (1986-2016) basin with additional geographic variables.
+#'
+#' The variables are as follows:
+#'
+#' \itemize{
+#'   \item storm.id: Unique storm ID. Constructed by basin (chars. 1-2) + ATCF cyclone number for that year (chars. 3-4) + Year (chars. 5-8).
+#'   \item storm.name: Name, if available, or else "UNNAMED".
+#'   \item n.obs: Number of best track entries.
+#'   \item storm.duration: Lifetime of the storm in seconds.
+#'   \item storm.pdi: Power-dissipation index. An indicator of the released energy of a hurricane, it integrates (cubic) wind speed over tropical-cyclone lifetime.
+#'   \item max.wind: supremum of the maximum sustained surface wind speed.
+#'   \item mean.wind: mean of the maximum sustained surface wind speed.
+#'   \item mean.sq.wind: mean of the squared maximum sustained surface wind speed.
+#'   \item storm.year: Year of storm occurrence.
+#'   \item basin: EPAC (Northeast Pacific), this column is kept for structure consistency.
+#'   \item sst: Annual and basin-wide averaged sea surface temperature.
+#'   \item sst.norm: Normalised SST.
+#'   \item sst.class: low for sst.norm < 1, high for sst.norm >= 1.
+#'   \item first.lat: Genesis latitude of the storm.
+#'   \item last.lat: Death latitude of the storm.
+#'   \item first.long: Genesis longitude of the storm.
+#'   \item last.long: Death latitude of the storm.
+#'   \item distance: Path length of the storm. Calculated using the Spherical Law of Cosines.
+#' }
+#'
+#' The hurricane data (variables 1-9, 14-18) comes from the HURDAT2, while the SST is calculated from the HadISST1 (variables 11-13).
+#'
+#' @docType data
+#' @keywords datasets
+#' @name tc.pdi.geog.epac
+#' @usage data(tc.pdi.geog.epac)
+#' @format A data frame with 594 rows and 18 variables
+#' @author Alfredo Hernández \email{aldomann.designs@gmail.com}
+#' @references HURDAT: \url{http://www.aoml.noaa.gov/hrd/hurdat/Data_Storm.html}, HadISST1: \url{https://www.metoffice.gov.uk/hadobs/hadisst/}
+NULL
